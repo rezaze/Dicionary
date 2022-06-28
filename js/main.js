88bc5f86c069a6e4bb3b16ac60b9ccec20f55ef5
rezaze <reza8zy1@gmail.com>
2022-06-29 02:53:18 +0430

    Dicionary

--- js/main.js
+++ js/main.js
@@ -10,11 +10,14 @@ const searchWord = $.getElementById("word"),
     showDefinition = $.getElementById("definition"),
     showExample = $.getElementById("example"),
     container = $.querySelector("#con")
-const voicePlay = $.getElementById("au");
+const voicePlay = $.getElementById("au"),
+    loadText = $.getElementById("load-text");
 
+let load = null;
 searchBtn.addEventListener("click", () => {
     let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord.value}`
 
+    loadingText();
 
     fetch(url)
         .then(response => response.json())
@@ -22,6 +25,8 @@ searchBtn.addEventListener("click", () => {
 
             setTimeout(() => {
                 showDescription(res)
+                loadText.innerHTML = "";
+                clearInterval(load);
             }, 1000)
 
         })
@@ -35,15 +40,21 @@ function showDescription(item) {
     let getFirsetWord = arrWord[0].toUpperCase();
     arrWord.shift();
     let newWord = getFirsetWord + arrWord.join("")
-    let i = 0;
+
     /////create word
+    let { definition, example } = item[0].meanings[0].definitions[0]
+
 
     let { audio, text } = item[0].phonetics[0];
+    if (!audio) {
+        item[0].phonetics.forEach(e => {
+            if (e.audio) {
+                audio = e.audio;
+            }
+        })
+    }
 
-    ///// create voice
 
-    let { definition, example } = item[0].meanings[0].definitions[0]
-    console.log(newWord);
     voicePlay.src = `${audio}`;
 
     voiceBtn.addEventListener("click", () => {
@@ -55,11 +66,18 @@ function showDescription(item) {
 
     showWords.innerHTML = newWord;
     showText.innerHTML = text;
-    console.log(example);
+
 
     if (!example) {
-        example = "";
+        item[0].meanings[0].definitions.forEach(e => {
+            if (e.example) {
+                example = e.example;
+            } else {
+                example = "";
+            }
+        })
     }
+
 
 
     container.insertAdjacentHTML("beforeend", `
@@ -69,10 +87,19 @@ function showDescription(item) {
 
 
     searchWord.value = "";
-    console.log(audio);
-    console.log(item);
 
 }
 searchWord.addEventListener("keyup",
     e => e.keyCode === 13 && searchBtn.click()
 )
+
+function loadingText() {
+    loadText.innerHTML = "Loading..."
+    load = setInterval(() => {
+        loadText.innerHTML = loadText.innerHTML.slice(0, loadText.innerHTML.length - 1);
+
+        if (loadText.innerHTML.length === 7) {
+            loadText.innerHTML = `Loading...`;
+        }
+    }, 400)
+}
